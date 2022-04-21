c200ee331381441e78190c674d38611e06d0f023
tasha-03 <tatyana.pavl03@gmail.com>
2022-04-21 21:58:31 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Work from "./pages/works/Work";
+import Works from "./pages/works/Works";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Users from "./pages/users/Users";
+import SignUp from "./pages/auth/SignUp";
+import LogIn from "./pages/auth/Login";
+import { Container } from "react-bootstrap";
+import useToken from "./hooks/useToken";
+import EmailConfirmation from "./pages/auth/EmailConfirmation";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <NavBar />
+        <Container>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="works">
+                <Route index element={<Navigate to="search" />} />
+                <Route path="search" element={<Works />} />
+                <Route path=":workId" element={<Work />} />
+              </Route>
+              <Route path="users">
+                <Route index element={<Navigate to="search" />} />
+                <Route path="search" element={<Users />} />
+                <Route path=":userLogin">
+                  <Route index element={<Navigate to="dashboard" />} />
+                  <Route path="dashboard" />
+                  <Route path="works">
+                    <Route index />
+                    <Route path="drafts" />
+                  </Route>
+                  <Route path="history" />
+                  <Route path="collections" />
+                  <Route path="series" />
+                </Route>
+              </Route>
+              <Route path="tags">
+                <Route index element={<Navigate to="search" />} />
+                <Route path="search" />
+                <Route path=":tagId" />
+              </Route>
+              <Route path="fandoms">
+                <Route index element={<Navigate to="category" />} />
+                <Route path="category">
+                  <Route path=":categoryName" />
+                </Route>
+                <Route path=":fandomId" />
+              </Route>
+              <Route path="about">
+                <Route index element={<Navigate to="socials" />} />
+                <Route path="FAQ" />
+                <Route path="socials" />
+              </Route>
+              <Route path="preferences">
+                <Route index />
+              </Route>
+              <Route path="login" element={<LogIn />} />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="confirm-email" element={<EmailConfirmation />} />
+            </Route>
+          </Routes>
+        </Container>
+        <Footer />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
